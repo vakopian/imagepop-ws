2dda25f529143b99f6a90626d61853660c9e55f3
Bernard Xie <bxie94@gmail.com>
2016-03-29 19:37:47 -0700

    added response body

--- src/main/java/com/imagepop/domain/EmailExistsException.java
+++ src/main/java/com/imagepop/domain/EmailExistsException.java
@@ -1,8 +1,13 @@
 package com.imagepop.domain;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Created by BernardXie on 3/21/16.
  */
+
+@ResponseStatus(value= HttpStatus.CONFLICT, reason="Duplicate Emails")
 public final class EmailExistsException extends RuntimeException{
     public EmailExistsException() {
         super();
