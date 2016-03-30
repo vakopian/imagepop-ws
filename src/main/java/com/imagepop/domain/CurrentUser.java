70dc395b364009f1930c5d9489c24468152ffd40
Nathan Yanaga <yanaga@usc.edu>
2016-03-29 22:19:32 -0700

    Added CurrentUser and CurrentUserDetailService

--- src/main/java/com/imagepop/domain/CurrentUser.java
+++ src/main/java/com/imagepop/domain/CurrentUser.java
@@ -1,7 +1,21 @@
 package com.imagepop.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import java.util.Collection;
+
 /**
  * Created by nyanaga on 3/29/16.
  */
-public class CurrentUser {
+public class CurrentUser extends User {
+
+    public CurrentUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+    }
+
+    public CurrentUser(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired,
+                       boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+    }
+
 }
