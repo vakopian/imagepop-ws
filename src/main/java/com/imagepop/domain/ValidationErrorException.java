10f35f9ba7035958f1f07c02aa1dc2a4f333efd7
Bernard Xie <bxie94@gmail.com>
2016-04-04 10:35:22 -0700

    modified request code

--- src/main/java/com/imagepop/domain/ValidationErrorException.java
+++ src/main/java/com/imagepop/domain/ValidationErrorException.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * Created by BernardXie on 3/21/16.
  */
 
-@ResponseStatus(value= HttpStatus.INTERNAL_SERVER_ERROR, reason="Validation Error")
+@ResponseStatus(value= HttpStatus.BAD_REQUEST, reason="Validation Error")
 public final class ValidationErrorException extends RuntimeException{
     public ValidationErrorException() {
         super();
