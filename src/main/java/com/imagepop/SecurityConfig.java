41acc55daf30f4cab5648c82ce4b40872b3bbba3
Nathan Yanaga <yanaga@usc.edu>
2016-04-04 10:32:14 -0700

    Revert "Add in Spring security"
    This reverts commit 0bc07f9c2dced478da1f48c3489aaa251087ed32.

--- src/main/java/com/imagepop/SecurityConfig.java
+++ src/main/java/com/imagepop/SecurityConfig.java
@@ -1,42 +1,7 @@
 package com.imagepop;
 
-import com.imagepop.domain.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-
 /**
  * Created by nyanaga on 3/29/16.
  */
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserRepository repo;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-
-                .authorizeRequests()
-                .antMatchers("/", "/landing").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // TODO Authenticate user and assign role here
-        // auth.inMemoryAuthentication().withUser("User").password("password").roles("USER");
-    }
+public class SecurityConfig {
 }
