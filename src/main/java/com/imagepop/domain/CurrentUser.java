2fe5756eabb91dd7791e35071b15f68e2176db1b
Nathan Yanaga <yanaga@usc.edu>
2016-04-04 10:31:25 -0700

    Revert "Added CurrentUser and CurrentUserDetailService"
    This reverts commit 70dc395b364009f1930c5d9489c24468152ffd40.

--- src/main/java/com/imagepop/domain/CurrentUser.java
+++ src/main/java/com/imagepop/domain/CurrentUser.java
@@ -1,21 +1,7 @@
 package com.imagepop.domain;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import java.util.Collection;
-
 /**
  * Created by nyanaga on 3/29/16.
  */
-public class CurrentUser extends User {
-
-    public CurrentUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
-        super(username, password, authorities);
-    }
-
-    public CurrentUser(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired,
-                       boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
-        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
-    }
-
+public class CurrentUser {
 }
--- src/main/java/com/imagepop/domain/CurrentUserDetailService.java
+++ src/main/java/com/imagepop/domain/CurrentUserDetailService.java
@@ -1,30 +1,7 @@
 package com.imagepop.domain;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import java.util.HashSet;
-
 /**
  * Created by nyanaga on 3/29/16.
  */
-public class CurrentUserDetailService implements UserDetailsService{
-    @Autowired
-    private UserRepository repository;
-
-    @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        User dbUser = repository.findByEmail(s);
-        HashSet<SimpleGrantedAuthority> authorities = new HashSet<>();
-        for (User.Role role: dbUser.getRoles()) {
-            authorities.add(new SimpleGrantedAuthority(role.toString()));
-        }
-        CurrentUser cu = new CurrentUser(dbUser.getPassword(), dbUser.getPassword(), authorities);
-
-        return cu;
-    }
+public class CurrentUserDetailService {
 }
